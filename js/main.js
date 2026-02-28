b0faf2f77e856956920e7e5d43b215cb71113949
Pavel Bogutskiy <Pavel.web0111@gmail.com>
2026-02-28 20:51:18 +0500

    JS: Menu switching

--- js/main.js
+++ js/main.js
@@ -2,16 +2,34 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+
+const LightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-dark";
+};
+const LightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-light";
+};
+const openMenu = (event) => {
+  //фенкция открывания меню
+  menu.classList.add("is-open"); //Вешает класс is-open
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden"; //Запрещаем прокрутку сайта под меню
+  LightModeOn();
+};
+const closeMenu = (event) => {
+  //фенкция закрывания меню
+  menu.classList.remove("is-open"); //Убирает класс is-open
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = ""; //Возвращает прокрутку сайта под меню
+  LightModeOff();
+};
+
 window.addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-dark";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-light";
-  }
+  this.scrollY > 1 ? LightModeOn() : LightModeOff();
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("is-open");
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
